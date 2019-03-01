e9169c54e6869961387b3b90024b3a4ca72c483c
Anmol Chabbra <anmolchabbra1@gmail.com>
2019-03-01 13:48:05 +0530

    shift left and right working

--- lib/src/main/java/edu/illinois/cs/cs125/spring2019/mp1/lib/Transform.java
+++ lib/src/main/java/edu/illinois/cs/cs125/spring2019/mp1/lib/Transform.java
@@ -3,49 +3,37 @@ package edu.illinois.cs.cs125.spring2019.mp1.lib;
 public class Transform {
     public static RGBAPixel[][] shiftRight(final RGBAPixel[][] originalImage,
                                            final int amount) {
-      //  RGBAPixel[][] shift = new RGBAPixel[originalImage.length][originalImage[0].length];
-        RGBAPixel[][] shift = RGBAPixel.copyArray(originalImage);
-        if (amount == 0) {
-            return originalImage;
-        } else if (amount < 0) {
-            return null;
-        } else if (originalImage == null) {
-            return originalImage;
-        } else {
-            for (int i = 0; i < originalImage.length; i++) {
-                for (int j = 0; j < originalImage[i].length; j++) {
-                    shift[i][j] = RGBAPixel.getFillValue();
-                    if (i + amount <= (originalImage.length - 1)) {
-                        shift[i + amount][j] = originalImage[i][j];
-                    }
-
-                }
-            }
-            RGBAPixel.printArray(shift);
-            return shift;
-        }
+        return shiftLeftRight(originalImage, -amount);
     }
 
     public static RGBAPixel[][] shiftLeft(RGBAPixel[][] originalImage,
+                                               int amount) {
+        return shiftLeftRight(originalImage, amount);
+    }
+
+    public static RGBAPixel[][] shiftLeftRight(RGBAPixel[][] originalImage,
                                           int amount) {
-        RGBAPixel[][] shift = RGBAPixel.copyArray(originalImage);
-        for (int i = 0; i < shift.length; i++) {
-            for (int j = 0; j < shift[i].length; j++) {
-                if (i - amount >= 0) {
-                    shift[i - amount][j] = originalImage[i][j];
-                }
-                if (shift[i][j] == null) {
-                    shift[i][j] = RGBAPixel.getFillValue();
+        if (amount == 0) {
+            return originalImage;
+        } else if (originalImage == null) {
+            return originalImage;
+        }
+        RGBAPixel[][] shift = new RGBAPixel[originalImage.length][originalImage[0].length];
+        for (int i = 0; i < originalImage.length; i++) {
+            for (int j = 0; j < originalImage[i].length; j++) {
+                shift[i][j] = RGBAPixel.getFillValue();
+                int originalImageRow = i + amount;
+                if (originalImageRow <= (originalImage.length - 1) && originalImageRow >= 0) {
+                    shift[i][j] = originalImage[originalImageRow][j];
                 }
             }
         }
-        // RGBAPixel.printArray(shift);
         return shift;
     }
 
     public static RGBAPixel[][] shiftUp(RGBAPixel[][] originalImage,
                                         int amount) {
-        RGBAPixel[][] shift = RGBAPixel.copyArray(originalImage);
+        RGBAPixel[][] shift = new RGBAPixel[originalImage.length][originalImage[0].length];
         for (int i = 0; i < shift.length; i++) {
             for (int j = 0; j < shift[i].length; j++) {
                 if (j - amount >= 0) {
@@ -78,100 +66,49 @@ public class Transform {
     }
 
     public static RGBAPixel[][] rotateRight(RGBAPixel[][] originalImage) {
-        RGBAPixel[][] rotate = RGBAPixel.copyArray(originalImage);
-        int i;
-        int j;
-          if (rotate.length == rotate[0].length) {
-              for (i = 0; i < rotate.length; i++) {
-                  for (int j = 0; j < rotate[i].length; j++) { 
-                      rotate[j][i] = originalImage[i][j];
-                      RGBAPixel[][] transpose = RGBAPixel.copyArray(rotate);
-                      rotate[rotate.length - i - 1][j] = transpose[i][j];
-                  }
-              }
-          } else {
-                if (rotate.length > rotate[j].length) {
-                    rotate[0][j] = RGBAPixel.getFillValue();
-                    rotate[rotate.length - 1][j] = RGBAPixel.getFillValue();
-                    if (i > 0 && i < rotate.length - 1) {
-                        rotate[j][i] = originalImage[i][j];
-                        RGBAPixel[][] transpose = RGBAPixel.copyArray(rotate);
-                        rotate[rotate.length - i - 1][j] = transpose[i][j];
-                    }
-                } else {
-                    rotate[i][1] = RGBAPixel.getFillValue();
-                    rotate[i][rotate[j].length - 1] = RGBAPixel.getFillValue();
-                    if (j > 0 && j < rotate.length - 1) {
-                        rotate[j][i] = originalImage[i][j];
-                        RGBAPixel[][] transpose = RGBAPixel.copyArray(rotate);
-                        rotate[rotate.length - i - 1][j] = transpose[i][j];
-                    }
-                }
+        return null;
     }
-        RGBAPixel.printArray(rotate);
-        return rotate;
-                }
 
     public static RGBAPixel[][] rotateLeft(RGBAPixel[][] originalImage) {
-        RGBAPixel[][] rotate = RGBAPixel.copyArray(originalImage);
-        if (rotate.length == rotate[0].length) {
-            for (int i = 0; i < rotate.length; i++) {
-                for (int j = 0; j < rotate[i].length; j++) {
-                    rotate[j][i] = originalImage[i][j];
-                    RGBAPixel[][] transpose = RGBAPixel.copyArray(rotate);
-                    rotate[j][rotate.length - i - 1] = transpose[i][j];
-                }
-            }
-        } else if (rotate.length > rotate[0].length) {
-            for (int i = 0; i < rotate.length; i++) {
-                for (int j = 0; j < rotate[i].length; j++) {
-                    rotate[0][j] = RGBAPixel.getFillValue();
-                    rotate[rotate.length - 1][j] = RGBAPixel.getFillValue();
-                    if (i > 0 && i < rotate.length - 1) {
-                        rotate[j][i] = originalImage[i][j];
-                        RGBAPixel[][] transpose = RGBAPixel.copyArray(rotate);
-                        rotate[i][rotate.length - i - 1] = transpose[i][j];
-                    }
-                }
-            }
-        } else {
-            for (int i = 0; i < rotate.length; i++) {
-                for (int j = 0; j < rotate[i].length; j++) {
-                    rotate[i][1] = RGBAPixel.getFillValue();
-                    rotate[i][rotate[j].length - 1] = RGBAPixel.getFillValue();
-                    if (j > 0 && j < rotate.length - 1) {
-                        rotate[j][i] = originalImage[i][j];
-                        RGBAPixel[][] transpose = RGBAPixel.copyArray(rotate);
-                        rotate[i][rotate.length - i - 1] = transpose[i][j];
-                    }
-                }
-            }
-        }
-        // RGBAPixel.printArray(rotate);
-        return rotate;
+        return null;
+
     }
 
     public static RGBAPixel[][] flipVertical(RGBAPixel[][] originalImage) {
-       // RGBAPixel[][] flip = new RGBAPixel[originalImage.length][originalImage[0].length];
-        RGBAPixel[][] flip = RGBAPixel.copyArray(originalImage);
-        for (int i = 0; i < originalImage.length; i++) {
-            for (int j = 0; j < originalImage[i].length; j++) {
-                flip[i][flip[0].length - j - 1] = originalImage[i][j];
-            }
-        }
-        return flip;
+        return null;
     }
 
 
     public static RGBAPixel[][] flipHorizontal(RGBAPixel[][] originalImage) {
-        RGBAPixel[][] flip = new RGBAPixel[originalImage.length][originalImage[0].length];
-        for (int i = 0; i < originalImage.length; i++) {
-            for (int j = 0; j < originalImage[i].length; j++) {
-                flip[flip.length - i - 1][j] = originalImage[i][j];
-            }
-        }
-        return flip;
+        return null;
     }
+
+
+
+    public static RGBAPixel[][] shrinkVertical(RGBAPixel[][] originalImage,
+                                               int amount) {
+        return null;
+    }
+
+    public static RGBAPixel[][] expandVertical(RGBAPixel[][] originalImage,
+                                               int amount)  {
+        return null;
+    }
+
+    public static RGBAPixel[][] shrinkHorizontal(RGBAPixel[][] originalImage,
+                                                 int amount) {
+        return null;
+    }
+
+    public static RGBAPixel[][] expandHorizontal(RGBAPixel[][] originalImage,
+                                                 int amount) {
+        return null;
+    }
+
+    public static RGBAPixel[][] greenScreen(RGBAPixel[][] originalImage) {
+        return null;
+    }
+
 }
 
 
